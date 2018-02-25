2a47348b26777cb48ff74d71e5374c08115143a3
kabyr <eugenepro@icloud.com>
2018-02-25 16:33:38 +0200

    upload

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,19 @@ $('.lessons__block a, .video a, .intro__course__block').on('click', function(eve
   $('.pop-up-video iframe').attr('src', link);
   $('.pop-up-video').fadeIn();
   $('body').css('overflow', 'hidden');
+  $('iframe')[0].src += '&autoplay=1';
 });
 
 $('.pop-up-close').on('click', function() {
   $('.pop-up').fadeOut();
   $('body').css('overflow', 'visible');
+  $('iframe').each(function(index) {
+    $(this).attr('src', '');
+    return false;
+  });
 });
 
 $('.log-in').on('click', function() {
   $('.pop-up-login').fadeIn();
 });
+
