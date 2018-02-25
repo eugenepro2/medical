10ddce0029469a17d991a81c9ae804f17bc88309
kabyr <eugenepro@icloud.com>
2018-02-25 13:02:18 +0200

    upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
-import sayHello from './lib/sayHello.js';
+$('.lessons__block a, .video a, .intro__course__block').on('click', function(event) {
+  event.preventDefault();
+  let link = $(this).attr('href');
+  $('.pop-up-video iframe').attr('src', link);
+  $('.pop-up-video').fadeIn();
+  $('body').css('overflow', 'hidden');
+});
 
-sayHello();
+$('.pop-up-close').on('click', function() {
+  $('.pop-up').fadeOut();
+  $('body').css('overflow', 'visible');
+});
+
+$('.log-in').on('click', function() {
+  $('.pop-up-login').fadeIn();
+});
